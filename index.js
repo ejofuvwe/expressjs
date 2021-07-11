ae4188ebf434b813ab94d38dc452a0ad9adaff87
ejofuvwe <ejofuvwe@gmail.com>
2021-07-11 05:51:34 +0100

    express ecommerce app

---
+++ index.js
@@ -0,0 +1,81 @@
+const {v4 :uuidv4 } = require('uuid');
+const express = require ('express');
+const { readSync } = require('fs');
+const app = express();
+const path = require('path')
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const products = require('./model/product')
+const PORT = 3000;
+
+
+
+// app to read
+ app.get('/', (req,res) => {
+     res.json(products)
+ })
+// app to identify product by id
+
+ app.get('/product/:id', (req,res) =>{
+     const found = products.find(product =>product.id === parseInt(req.params.id))
+     if(found){
+         res.jsonp(products.find(product =>product.id === parseInt(req.params.id)))
+     }
+     else{ 
+         res.status(404).send(`product not found with id :${parseInt(req.params.id)}`)
+     }
+    })
+     // post data
+
+app.post('/product', (req, res) =>{
+
+    const newProduct = {
+        id: uuidv4(),
+        name: req.body.name,
+        description: req.body.description,
+        image: req.body.image,
+        price: req.body.price
+    }
+    products.push(newProduct);
+    res.json(products)
+});
+
+app.put('/product/:id', (req,res) =>{
+    const found = products.find(product =>product.id === parseInt(req.params.id))
+    if (found) {
+        const updProduct = req.body;
+        products.forEach(product => {
+            if (product.id === parseInt(req.params.id)) {
+                product.name = updProduct.name ? updProduct.name:product.name;
+                product.description = updProduct.description ? updProduct.description:product.description;
+                product.image = updProduct.image ? updProduct.image:product.image;
+                product.price = updProduct.price ? updProduct.price:product.price;
+
+                res.json({msg:'product updated', product});
+            }
+        })
+    }
+    else{ 
+        res.status(400).send(`product not found with id :${parseInt(req.params.id)}`)
+    }
+   });
+
+
+   app.delete('/product/:id', (req,res) =>{
+    const found = products.filter(product =>product.id !== parseInt(req.params.id))
+    if(found){
+        res.jsonp(products.filter(product =>product.id === parseInt(req.params.id)))
+    }
+    else{ 
+        res.status(404).send(`product not found with id :${parseInt(req.params.id)}`)
+    }
+   })
+   
+
+
+app.listen(PORT, ()=>{
+    console.log(`server starting on port http://127.0.0.1:${PORT}`);
+})
---
+++ model/product.js
@@ -0,0 +1,28 @@
+const products = [
+    {
+        id: 123,
+        name: 'iphone',
+        description: 'purple',
+        image: 'iphone',
+        price: 400000
+    },
+
+    { 
+        id: 456,
+        name: 'iron',
+        description: 'gray',
+        image: 'iron',
+        price: 10000
+
+    },
+    {
+        id: 789,
+        name: 'touch',
+        description: 'white',
+        image:'tou',
+        price:4000
+    },
+];
+
+module.exports = products;
+
